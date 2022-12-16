8ba48c992d6c09c7f3b9ce1890763dba5ddfc123
Siddhartha Patki <siddharthapatki@gmail.com>
2022-12-16 08:15:59 +0530

    first commit

---
+++ NUnit-BrowserStack/BrowserStackNUnitTest.cs
@@ -0,0 +1,113 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using BrowserStack;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace BrowserStack
+{
+    [TestFixture]
+    public class BrowserStackNUnitTest
+    {
+        protected IWebDriver driver;
+        protected string profile;
+        protected string environment;
+        private Local browserStackLocal;
+
+        public BrowserStackNUnitTest(string profile, string environment)
+        {
+            this.profile = profile;
+            this.environment = environment;
+        }
+
+        static DriverOptions getBrowserOption(String browser)
+        {
+            switch (browser)
+            {
+                case "chrome":
+                    return new OpenQA.Selenium.Chrome.ChromeOptions();
+                case "firefox":
+                    return new OpenQA.Selenium.Firefox.FirefoxOptions();
+                case "safari":
+                    return new OpenQA.Selenium.Safari.SafariOptions();
+                case "edge":
+                    return new OpenQA.Selenium.Edge.EdgeOptions();
+                default:
+                    return new OpenQA.Selenium.Chrome.ChromeOptions();
+            }
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            NameValueCollection caps =
+                ConfigurationManager.GetSection("capabilities/" + profile) as NameValueCollection;
+            NameValueCollection settings =
+                ConfigurationManager.GetSection("environments/" + environment)
+                    as NameValueCollection;
+            DriverOptions capability = getBrowserOption(settings["browser"]);
+
+            capability.BrowserVersion = "latest";
+            System.Collections.Generic.Dictionary<string, object> browserstackOptions =
+              new Dictionary<string, object>();
+
+            foreach (string key in caps.AllKeys)
+            {
+                browserstackOptions.Add(key, caps[key]);
+            }
+
+            String username = Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME");
+            if (username == null)
+            {
+                username = ConfigurationManager.AppSettings.Get("user");
+            }
+
+            String accesskey = Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY");
+            if (accesskey == null)
+            {
+                accesskey = ConfigurationManager.AppSettings.Get("key");
+            }
+
+            browserstackOptions.Add("userName", username);
+            browserstackOptions.Add("accessKey", accesskey);
+
+            if (caps.Get("local").ToString() == "true")
+            {
+                browserStackLocal = new Local();
+                List<KeyValuePair<string, string>> bsLocalArgs = new List<KeyValuePair<string, string>>();
+                // Starts the Local instance with the required arguments
+                bsLocalArgs.Add(new KeyValuePair<string, string>("key", accesskey));
+                //bsLocalArgs.Add(new KeyValuePair<string, string>("binarypath", "/Users/siddharthapatki/Downloads/BrowserStackLocal"));
+                bsLocalArgs.Add(new KeyValuePair<string, string>("v", "true"));
+                bsLocalArgs.Add(new KeyValuePair<string, string>("logfile", "./logs.txt"));
+                bsLocalArgs.Add(new KeyValuePair<string, string>("forcelocal", "true"));
+                bsLocalArgs.Add(new KeyValuePair<string, string>("-use-system-installed-ca", "true"));
+                bsLocalArgs.Add(new KeyValuePair<string, string>("localIdentifier", "HelloWorllldd"));
+
+
+                // Starts the Local instance with the required arguments
+                browserStackLocal.start(bsLocalArgs);
+
+
+            }
+            capability.AddAdditionalOption("bstack:options", browserstackOptions);
+            driver = new RemoteWebDriver(
+              new Uri("http://" + ConfigurationManager.AppSettings.Get("server") + "/wd/hub/"),
+              capability
+            );
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+            if (browserStackLocal != null)
+            {
+                browserStackLocal.stop();
+            }
+        }
+    }
+}
---
+++ NUnit-BrowserStack/LocalTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Text.RegularExpressions;
+
+namespace BrowserStack
+{
+  [TestFixture("local", "chrome")]
+  public class LocalTest : BrowserStackNUnitTest
+  {
+    public LocalTest(string profile, string environment) : base(profile, environment) { }
+
+    [Test]
+    public void HealthCheck()
+    {
+      driver.Navigate().GoToUrl("http://localhost:45454/");
+      Assert.IsTrue(Regex.IsMatch(driver.Title, "BrowserStack Local", RegexOptions.IgnoreCase));
+    }
+  }
+}
